1c510ee1af219e056350c486186f02df0dade182
Stephen Stewart <stephen.stewart@carisenda.com>
2012-05-08 15:30:39 +0100

    fix regex for hashbangs

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var ircLib = require('irc'),
 
     config = {
       server: 'irc.perl.org',
-      channel: '#testparrot',
-      name: 'parrot'
+      channel: '#cabal',
+      name: 'twitterbot'
     },
 
     client = new ircLib.Client(config.server, config.name, {
@@ -15,7 +15,7 @@ var ircLib = require('irc'),
     }),
 
     reName = new RegExp('^' + config.name, 'i'),
-    reUrl = /((?:http|https):\/\/[a-z0-9\/\?=_#&%~-]+(\.[a-z0-9\/\?=_#&%~-]+)+)|(www(\.[a-z0-9\/\?=_#&%~-]+){2,})/gi,
+    reUrl = /((?:http|https):\/\/[a-z0-9\/\?=_#&%~-]+(\.[a-z0-9\/\?=_#!&%~-]+)+)|(www(\.[a-z0-9\/\?=_#&%~-]+){2,})/gi,
     reTwitterId = /\/(\d+)$/;
 
 console.log('server: ' + config.server);
@@ -29,38 +29,44 @@ client.addListener('message', function(from, to, message) {
 
     var link = reUrl.exec(message);
 
-    if (link) {
+    if (link && link.length) {
+    console.log('got a link');
 
       //set link to the matched string 
+console.log(link);
       link = link[1];
 
       var twitterUrl = new url.parse(link),
           id = reTwitterId.exec(link);
 
-      id = id[1];
+console.log(id);
+      if (id && id.length) {
+        console.log('got an id');
+        id = id[1];
 
-      var options = {
-        host: 'api.twitter.com',
-        port: twitterUrl.port,
-        path: '/1/statuses/show/' + id + '.json'
-      };
+        var options = {
+          host: 'api.twitter.com',
+          port: twitterUrl.port,
+          path: '/1/statuses/show/' + id + '.json'
+        };
 
-      var request = http.get(options, function(res) {
-        var data = '';
+        var request = http.get(options, function(res) {
+          var data = '';
 
-        res.on('data', function(chunk) {
-          data += chunk;
+          res.on('data', function(chunk) {
+            data += chunk;
+          });
+
+          res.on('end', function() {
+            var tweet = JSON.parse(data);
+            //console.log(tweet);
+            client.say(config.channel, 'Tweet from: ' + tweet.user.screen_name);
+            client.say(config.channel, tweet.text);
+          });
+
         });
 
-        res.on('end', function() {
-          var tweet = JSON.parse(data);
-          //console.log(tweet);
-          client.say(config.channel, 'Tweet from: ' + tweet.user.screen_name);
-          client.say(config.channel, tweet.text);
-        });
-
-      });
-      
+      }
     }
   }
 
